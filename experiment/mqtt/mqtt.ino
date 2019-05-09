2b01bbfb784200f0a185708b7b71ec69153b6870
tbureete <tbureete@purdue.edu>
2019-05-09 16:16:52 -0400

    make connection for generic

--- experiment/mqtt/mqtt.ino
+++ experiment/mqtt/mqtt.ino
@@ -1,55 +1,42 @@
 #include "EspMQTTClient.h"
 
-
+const char* robotName = "i-robot07";
+const char* wifiSSID = "ME588G4";
+const char* wifiPassword = "1n1t1al0";
+const char* brokerIP = "192.168.1.147";
+const short brokerPort = 8883;
+const char* robotID = "07";
 
 EspMQTTClient client(
-
-  "ME588G4",
-  "1n1t1al0",
-  "192.168.1.147",   
-  "i-robot07",
-  8883              
+  wifiSSID,
+  wifiPassword,
+  brokerIP,   
+  robotName,
+  brokerPort              
 );
 
-
+char payload[30];
+char commandTopic[30];
 
 void setup()
-
 {
-
   Serial.begin(115200);
-
-
-
-  // Optionnal functionnalities of EspMQTTClient : 
-  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
-//  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overrited with enableHTTPWebUpdater("user", "password").
-//  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
-
+  client.enableDebuggingMessages();
 }
 
 void onConnectionEstablished()
-
 {
-
-  // Subscribe to "mytopic/test" and display received message to Serial
-  client.subscribe("irobot/test", [](const String & payload) {
+  sprintf(commandTopic, "irobot/command/%s", robotID);
+  client.subscribe(commandTopic, [](const String & payload) {
+    Serial.print("Recieve message: ");
     Serial.println(payload);
   });
-  
-  client.publish("irobot/test", "This is a message"); // You can activate the retain flag by setting the third parameter to true
 
-  client.executeDelayed(5 * 1000, []() {
-    client.publish("irobot/test", "This is a message sent 5 seconds later");
-  });
-
+  sprintf(payload, "%s is connected", robotName);
+  client.publish("irobot/feedback", payload); 
 }
-
-
 
 void loop()
 {
-
   client.loop();
-
 }
