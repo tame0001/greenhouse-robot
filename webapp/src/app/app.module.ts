b2ad2b1358aa63de3ddd68e0b1e7a4f68afbc1f5
tbureete <tbureete@purdue.edu>
2019-08-22 23:00:06 -0400

    add new component

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LineSensorComponent } from './line-sensor/line-sensor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineSensorComponent
   ],
   imports: [
     BrowserModule,
---
+++ webapp/src/app/line-sensor/line-sensor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line-sensor',
+  templateUrl: './line-sensor.component.html',
+  styleUrls: ['./line-sensor.component.css']
+})
+export class LineSensorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
