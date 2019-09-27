4184a3c944abc41c8f656355b5019f11d08a9b4d
Thirawat Bureetes <tbureete@purdue.edu>
2019-09-27 00:09:05 -0400

    able to get message from broker

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LineSensorComponent } from './line-sensor/line-sensor.component';
 
+import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '128.46.109.133',
+  port: 9001,
+  path: '/'
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +20,8 @@ import { LineSensorComponent } from './line-sensor/line-sensor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webapp/src/app/line-sensor/line-sensor.component.ts
+++ webapp/src/app/line-sensor/line-sensor.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { MqttService, IMqttMessage } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-line-sensor',
   templateUrl: './line-sensor.component.html',
   styleUrls: ['./line-sensor.component.css']
 })
-export class LineSensorComponent implements OnInit {
 
-  constructor() { }
+export class LineSensorComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  public message: string;
+
+  constructor(private mqttService: MqttService) {
+    this.subscription = this.mqttService.observe('irobot/parameters').subscribe((
+      message: IMqttMessage) => {
+        this.message = message.payload.toString();
+        console.log(this.message)
+      });
+   }
 
   ngOnInit() {
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
