8f84bc93bfc7968c63c7b49f267350bd60f02e86
Thirawat Bureetes <tbureete@purdue.edu>
2019-09-29 01:20:13 -0400

    Update parameters with detail

--- webapp/src/app/line-sensor/line-sensor.component.ts
+++ webapp/src/app/line-sensor/line-sensor.component.ts
@@ -9,15 +9,41 @@ import { MqttService, IMqttMessage } from 'ngx-mqtt';
 })
 
 export class LineSensorComponent implements OnInit, OnDestroy {
-  private subscription: Subscription;
-  public message: string;
+  private parameterSub: Subscription;
+  private feedbackSub: Subscription;
+  public mqttMessage: string;
+  public currentStatus: string;
+  public splitedMessage: any;
+  public robotID: string;
+  public letfSpeed: string;
+  public rightSpeed: string;
 
   constructor(private mqttService: MqttService) {
-    this.subscription = this.mqttService.observe('irobot/parameters').subscribe((
+
+    this.parameterSub = this.mqttService.observe('irobot/parameters').subscribe((
       message: IMqttMessage) => {
-        this.message = message.payload.toString();
-        console.log(this.message);
+        this.mqttMessage = message.payload.toString();
+        console.log(this.mqttMessage);
+        this.splitedMessage = this.mqttMessage.split(':');
+        console.log(this.splitedMessage);
+        this.robotID = this.splitedMessage[0];
+        this.letfSpeed = this.splitedMessage[1];
+        this.rightSpeed = this.splitedMessage[2];
       });
+
+    this.feedbackSub = this.mqttService.observe('irobot/feedback').subscribe((
+        message: IMqttMessage) => {
+          this.mqttMessage = message.payload.toString();
+          console.log(this.mqttMessage);
+          this.splitedMessage = this.mqttMessage.split(':');
+          this.robotID = this.splitedMessage[0];
+          if (this.splitedMessage[1] === '1') {
+            this.currentStatus = 'RUNNING';
+          } else {
+            this.currentStatus = 'STOPPING';
+          }
+
+        });
    }
 
   sendRun() {
@@ -28,7 +54,12 @@ export class LineSensorComponent implements OnInit, OnDestroy {
     this.unsafePublish('irobot/command/17', 's');
   }
 
+  sendPing() {
+    this.unsafePublish('irobot/command/17', 'p');
+  }
+
   ngOnInit() {
+    this.sendPing()
   }
 
   public unsafePublish(topic: string, message: string): void {
@@ -36,7 +67,7 @@ export class LineSensorComponent implements OnInit, OnDestroy {
   }
 
   public ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.parameterSub.unsubscribe();
   }
 
 }
