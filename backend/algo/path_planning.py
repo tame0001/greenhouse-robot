1cd19f4113aee6fe92c11dcb17f2ae72b6120f0d
Bureetes <tbureete@purdue.edu>
2020-03-05 17:43:08 -0500

    generate command seq

--- backend/algo/path_planning.py
+++ backend/algo/path_planning.py
@@ -9,10 +9,39 @@ def gen_movement_cmd(start, stop, dirrection):
 
     commands = []
 
-    if dirrection
+    if abs(x_distance) > 0:
 
-    for i in range(x_distance):
-        commands.append('F')
+        if (dirrection == 1 and x_distance < 0) or (dirrection == 3 and x_distance > 0):
+            commands.append('T')
+
+        elif (dirrection == 2 and x_distance < 0) or (dirrection == 4 and x_distance > 0):
+            commands.append('Q')
+
+        elif (dirrection == 2 and x_distance > 0) or (dirrection == 4 and x_distance < 0):
+            commands.append('E')
+
+        for i in range(abs(x_distance)):
+            commands.append('R')
+
+        dirrection = 1 if x_distance > 0 else 3
+
+
+    if abs(y_distance) > 0:
+
+        if (dirrection == 2 and y_distance < 0) or (dirrection == 4 and y_distance > 0):
+            commands.append('T')
+
+        elif (dirrection == 1 and y_distance > 0) or (dirrection == 3 and y_distance < 0):
+            commands.append('Q')
+
+        elif (dirrection == 1 and y_distance < 0) or (dirrection == 3 and y_distance > 0):
+            commands.append('E')
+
+        for i in range(abs(y_distance)):
+            commands.append('R')
+
+        dirrection = 2 if y_distance > 0 else 4
+        
 
     print(commands)
 
@@ -28,6 +57,6 @@ if __name__ == '__main__':
     stop = (x2, y2)
     # print(stop)
 
-    dirrection = int(input('X axis direction (1, 2, 3, 4): '))
+    dirrection = int(input('Direction (1, 2, 3, 4): '))
 
     gen_movement_cmd(start, stop, dirrection)
