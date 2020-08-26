3e87df3bf0d16b76075b9442edeffd4e7980c5ec
Bureetes <tbureete@purdue.edu>
2020-08-26 17:54:47 -0400

    create and query db

---
+++ backend/algo/crud.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import db, Position
+
+import datetime
+
+# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+DATABASE_URI = 'postgres+psycopg2://postgres@128.46.109.133:5432/mydb'
+
+engine = create_engine(DATABASE_URI)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+postion = Position(
+    robot = 10,
+    x = 3,
+    y = 5,
+    timestamp = datetime.datetime.now()
+)
+
+# session.add(postion)
+# session.commit()
+
+positions = session.query(Position).filter(Position.robot==3).order_by(Position.timestamp.desc()).first()
+print(positions)
+
+session.close()
+
+# db.metadata.create_all(engine)
